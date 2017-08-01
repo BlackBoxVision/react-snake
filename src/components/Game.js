eb03e1e08c9048f57a4c62c12e8856bcec5688cb
jonatansalas <jonatansalas@live.com.ar>
2017-08-01 16:04:44 -0300

    Minor changes

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Box from './Box';
 
@@ -7,24 +8,23 @@ export default class Game extends React.Component {
     state = {
         initialLength: 0,
         positions: [],
-        direction: {
+        direction: {},
+        foodPosition: {
             x: 0,
             y: 0
         }
     };
 
     tick = () => {
-        const value = Math.floor(Math.random() * 500);
+        const x = Math.floor(Math.random() * 25);
+        const y = Math.floor(Math.random() * 25);
 
         this.setState(state => ({
-            direction: {
-                x: value,
-                y: value
-            }
+            foodPosition: { x, y }
         }));
     };
 
-    startLoop = (updaterFunc, millis = 500) => {
+    startLoop = (updaterFunc, millis = 1000) => {
         const intervalId = setInterval(updaterFunc, millis);
 
         return () => clearInterval(intervalId);
@@ -39,13 +39,51 @@ export default class Game extends React.Component {
     }
 
     render() {
-        const { direction: { x, y } } = this.state;
+        const { foodPosition: { x, y } } = this.state;
 
         return (
-            <Box width={550} height={550} color="#414141">
-                <Box width={25} height={25} color="red" left={x} top={y} position="relative" />
+            <BoardProvider width={625} height={625} xBlocks={25} yBlocks={25} backgroundColor="#414141">
+                <Box width={25} height={25} color="red" left={x * 25} top={y * 25} position="relative" />
                 <Box width={25} height={25} color="green" />
-            </Box>
+            </BoardProvider>
         );
+    }
+}
+
+class BoardProvider extends React.Component {
+    static propTypes = {
+        width: PropTypes.number,
+        height: PropTypes.number,
+        xBlocks: PropTypes.number,
+        yBlocks: PropTypes.number,
+        backgroundColor: PropTypes.string
+    };
+
+    static contextTypes = {
+        width: PropTypes.number,
+        height: PropTypes.number,
+        xBlocks: PropTypes.number,
+        yBlocks: PropTypes.number
+    };
+
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    getContextTypes() {
+        return {
+            width: this.props.width,
+            height: this.props.height,
+            xBlocks: this.props.xBlocks,
+            yBlocks: this.props.yBlocks
+        }
+    }
+
+    render() {
+        return (
+            <Box width={this.props.width} height={this.props.height} color={this.props.backgroundColor}>
+                {this.props.children}
+            </Box>
+        )
     }
 }
