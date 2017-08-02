25beb7d14f342d06c7380f91b4ce53b8045b7b66
jonatansalas <jonatansalas@live.com.ar>
2017-08-02 11:32:34 -0300

    Added propTypes validation to Snake component

--- src/components/entities/Snake.js
+++ src/components/entities/Snake.js
@@ -3,11 +3,24 @@ import React from 'react';
 import Pixel from '../primitives/Pixel';
 
 export default class Snake extends React.Component {
+    static propTypes = {
+        head: PropTypes.shape({
+            x: PropTypes.number.isRequired,
+            y: PropTypes.number.isRequired
+        }),
+        tail: PropTypes.arrayOf({
+            x: PropTypes.number.isRequired,
+            y: PropTypes.number.isRequired
+        })
+    };
+
     render() {
+        const { head, tail } = this.props;
+
         return (
             <div>
-                <Pixel color="green" position={this.props.head} />
-                {this.props.tail.map(it => <Pixel color="green" position={it}/>)}
+                <Pixel color="green" position={head} />
+                {tail.map(it => <Pixel color="green" position={it}/>)}
             </div>
         );
     }
