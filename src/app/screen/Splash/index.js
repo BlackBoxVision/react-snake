1e6be8c91b8993027d488f2f5af84d03f69a08d4
jonatansalas <jonatansalas@live.com.ar>
2017-08-05 10:42:44 -0300

    Finish refactor to newer material-ui

--- src/app/screen/Splash/index.js
+++ src/app/screen/Splash/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'react-router-dom/Link';
 
 import Button from 'material-ui/Button';
+import Content from 'material-ui/Card/CardContent';
 
 import Layout from '../../common/Layout';
 import Container from '../../common/Container';
@@ -15,36 +16,38 @@ export default class Splash extends React.Component {
         return (
             <Layout>
                 <img src={AppLogo} alt="game logo" width="85%" style={styles.img} />
+                <Content>
                 <Container margin="75px" padding="10px 0px 0px 0px">
                     <Container margin="10px 0px 0px 0px">
-                        <Link to="/game">
+                        <Link to="/game" style={styles.link}>
                             <Button raised style={styles.button}>
                                 PLAY
                             </Button>
                         </Link>
                     </Container>
                     <Container margin="10px 0px 0px 0px">
-                        <Link to="/">
+                        <Link to="/" style={styles.link}>
                             <Button raised style={styles.button}>
                                 INSTRUCTIONS
                             </Button>
                         </Link>
                     </Container>
                     <Container margin="10px 0px 0px 0px">
-                        <Link to="/">
+                        <Link to="/" style={styles.link}>
                             <Button raised style={styles.button}>
                                 HIGH SCORE
                             </Button>
                         </Link>
                     </Container>
                     <Container margin="10px 0px 0px 0px">
-                        <Link to="/">
+                        <Link to="/" style={styles.link}>
                             <Button raised style={styles.button}>
                                 RATE
                             </Button>
                         </Link>
                     </Container>
                 </Container>
+                </Content>
             </Layout>
         );
     }
--- src/app/screen/Splash/styles/index.js
+++ src/app/screen/Splash/styles/index.js
@@ -1,11 +1,16 @@
 export default {
     button: {
         height: 50,
-        width: '100%'
+        width: '100%',
+        backgroundColor: '#69F0AE'
     },
     img: {
         paddingTop: 50,
         paddingLeft: 50,
         paddingRight: 50
+    },
+    link: {
+        color: 'white',
+        textDecoration: 'none'
     }
 };
